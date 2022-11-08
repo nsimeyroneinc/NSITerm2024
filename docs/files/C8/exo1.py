69aa0b520c30730bebf8bbf8d958051b9023e49b
nsimeyroneinc <nsi.meyroneinc@gmail.com>
2022-11-08 18:55:26 +0100

    08/11

--- docs/files/C8/exo1.py
+++ docs/files/C8/exo1.py
@@ -1,11 +1,20 @@
 def maxi(liste):
-    i_centre=len(liste)
-    l1=liste[i_centre:]
-    l2=liste[:i_centre]
+    if len(liste)==1:
+        return liste[0]
+    i_centre=(len(liste))//2
+    l1=liste[:i_centre]
+    l2=liste[i_centre:]
     m1=maxi(l1)
     m2=maxi(l2)
-    return m1,m2
+    if m1>m2:
+        return m1
+    else: 
+        return m2
 
-L=[12,19,10,13,11,15,9,14]
+
+        
+        
+
+L=[12,19,120,33,11,15,9,14,57]
 
 print(maxi(L))
