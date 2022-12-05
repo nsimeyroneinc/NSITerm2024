56c057fd1d0bbeae08bfb88ee6ab734c9dc23c76
nsimeyroneinc <nsi.meyroneinc@gmail.com>
2022-12-05 05:45:10 +0100

    05/12

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def define_env(env):
         8 : ["algorithmique","Diviser pour régner",1,"Algo/T5_3_Diviser_pour_regner.md"],
         9 : ["sd","Les Dictionnaires - Révision",1,"StructureDonnees/T4_1_Dictionnaires.md"],
         10 : ["sd","Les arbres",2,"StructureDonnees/T3_1_arbre.md"],
-        11: ["algorithmique","Algorithmes sur les arbres",2,"Algo/TP5_5_Implementation_arbres.md.md"]
+        11: ["algorithmique","Algorithmes sur les arbres",2,"Algo/TP5_5_Implementation_arbres.md"]
         12: ["algorithmique","arbres binaires de recherche",1,"StructureDonnees/T3_3_arbre_recherche.md"]
         #6 : ["os","Protocole de Routage",1,""],
         #7 : ["python","Notions de programmation orienté objet",2,"poo.md"],
