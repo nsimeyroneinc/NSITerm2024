087c10751bbda4ca40284b63073d1abec73d3962
nsimeyroneinc <nsi.meyroneinc@gmail.com>
2023-03-16 07:20:06 +0100

    16/03

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def define_env(env):
 
     @env.macro
     def capytale(id):
-        lien = "[![logo capytale](../images/capytale.png){.imgcentre width=150px border=2px}]"
+        lien = "[![logo capytale](./images/capytale.png){.imgcentre width=150px border=2px}]"
         lien +=f"(https://capytale2.ac-paris.fr/web/c/{id})"
         lien += "{target=_blank}"
         return lien
